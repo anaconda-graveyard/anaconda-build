f88cd4cc6c5a1c1e6c33c5d14544242ec4c149cf
Aaron Meurer <asmeurer@gmail.com>
2014-07-01 12:12:54 -0500

    Add __future__ imports to binstar_build_client/tests
    And clear some trailing whitespace.

--- binstar_build_client/tests/runner.py
+++ binstar_build_client/tests/runner.py
@@ -3,6 +3,10 @@ Created on Sep 23, 2013
 
 @author: sean
 '''
+
+from __future__ import (print_function, unicode_literals, division,
+    absolute_import)
+
 from unittest.runner import TextTestRunner, TextTestResult
 from unittest.signals import registerResult
 import time
@@ -86,7 +90,7 @@ class ColorTextTestRunner(TextTestRunner):
 
     def __init__(self, stream=sys.stderr, descriptions=True, verbosity=1,
         failfast=False, buffer=False, resultclass=ColorTextTestResult):
-        
+
         TextTestRunner.__init__(self, stream=stream, descriptions=descriptions,
                                 verbosity=verbosity, failfast=failfast, buffer=buffer,
                                 resultclass=resultclass)
@@ -112,15 +116,15 @@ class ColorTextTestRunner(TextTestRunner):
         if hasattr(result, 'separator2'):
             self.stream.writeln(result.separator2)
         run = result.testsRun
-        self.stream.writeln("Ran %d test%s in %.3fs" % 
+        self.stream.writeln("Ran %d test%s in %.3fs" %
                             (run, run != 1 and "s" or "", timeTaken))
         self.stream.writeln()
 
 
         return result
-    
+
     def write_end(self, result, coverage):
-        
+
         expectedFails = unexpectedSuccesses = skipped = 0
         try:
             results = map(len, (result.expectedFailures,
@@ -130,7 +134,7 @@ class ColorTextTestRunner(TextTestRunner):
             pass
         else:
             expectedFails, unexpectedSuccesses, skipped = results
-            
+
         infos = []
         if not result.wasSuccessful():
             self.stream.write('Tests: ' + red("FAILED"))
@@ -149,15 +153,13 @@ class ColorTextTestRunner(TextTestRunner):
             infos.append("unexpected successes=%d" % unexpectedSuccesses)
         if infos:
             self.stream.write(" (%s)" % (", ".join(infos),))
-            
+
         perc = coverage.pc_covered
         color = green
         if perc < 80:
             color = blue
         if perc < 50:
             color = red
-        
+
         cv = color("%i%%" % (int(coverage.pc_covered)))
         self.stream.write(", Coverage: %s \n" % (cv))
-        
-        
--- binstar_build_client/tests/runtests.py
+++ binstar_build_client/tests/runtests.py
@@ -3,7 +3,10 @@ Created on Aug 5, 2013
 
 @author: sean
 '''
-from __future__ import print_function
+
+from __future__ import (print_function, unicode_literals, division,
+    absolute_import)
+
 import unittest
 from os.path import dirname,join
 from argparse import ArgumentParser
@@ -11,17 +14,17 @@ from binstar_client.tests.runner import ColorTextTestRunner
 from binstar_client.tests.coverage_report import report
 
 def main():
-    
+
     parser = ArgumentParser()
     parser.add_argument('--html', action='store_true')
     parser.add_argument('source_dir', nargs='?', default='')
     args = parser.parse_args()
-    
+
     import coverage
     cov = coverage.coverage(include='**%s**' % args.source_dir if args.source_dir else '**/binstar_client/**',
                             omit=['**/lib/python2.7/**', '**/site-packages/**', '**/tests/**',
                                   ])
-    
+
     cov.start()
     import binstar_client
     print(binstar_client)
@@ -30,17 +33,17 @@ def main():
     print('Discover %s' % discover_dir)
     tests = loader.discover(discover_dir)
     runner = ColorTextTestRunner(verbosity=2)
-    result = runner.run(tests) 
+    result = runner.run(tests)
     cov.stop()
     cov.save()
     total = report(cov)
     if args.html:
         cov.html_report(directory='htmlcov')
-    
+
     runner.write_end(result, total)
-    
+
     exit(0 if result.wasSuccessful() else -1)
-    
-    
+
+
 if __name__ == '__main__':
     main()
