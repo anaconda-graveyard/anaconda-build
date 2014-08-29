58dea09243d788d31b767b30bd2e4175747631c6
srossross <srossross@gmail.com>
2014-08-29 08:49:55 -0700

    FIX: subcommands

--- binstar_build_client/commands/__init__.py
+++ binstar_build_client/commands/__init__.py
@@ -1,6 +1,5 @@
 
-from __future__ import (print_function, unicode_literals, division,
-    absolute_import)
+from __future__ import (print_function, unicode_literals, division)
 
 import imp
 import os
