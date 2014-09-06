88781963a6420128122be23d1d19fabd9df280aa
srossross <srossross@gmail.com>
2014-09-06 13:01:17 +0200

    TEST: added worker script test

---
+++ binstar_build_client/tests/test_worker_script.py
@@ -0,0 +1,29 @@
+'''
+Created on Feb 18, 2014
+
+@author: sean
+'''
+
+from __future__ import (print_function, unicode_literals, division,
+    absolute_import)
+
+from mock import patch
+import unittest
+
+from binstar_build_client.scripts.build import main
+from binstar_client.tests.fixture import CLITestCase
+from binstar_client.tests.urlmock import urlpatch
+
+
+class Test(CLITestCase):
+
+
+    @urlpatch
+    @patch('binstar_build_client.commands.worker.Worker')
+    def test_worker_simple(self, urls, Worker):
+
+        main(['--show-traceback', 'worker', '-u', 'username', 'build-queue-1'], False)
+        self.assertEqual(Worker().work_forever.call_count, 1)
+
+if __name__ == '__main__':
+    unittest.main()
