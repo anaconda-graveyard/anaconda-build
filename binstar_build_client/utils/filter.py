53644f2510e49da951f65af50f6904c569b7763e
srossross <srossross@gmail.com>
2014-11-05 09:12:26 -0800

    ENH: filter out files that are not in the current repo

--- binstar_build_client/utils/filter.py
+++ binstar_build_client/utils/filter.py
@@ -1,42 +1,30 @@
-'''
-Created on May 8, 2014
-
-@author: sean
-'''
-
 
 from __future__ import (print_function, unicode_literals, division,
     absolute_import)
 
-from fnmatch import fnmatch
 import os
+from subprocess import check_output, CalledProcessError
 
 class ExcludeGit(object):
     def __init__(self, path, use_git_ignore=True):
         self.path = os.path.abspath(path)
-        self.to_ignore = []
-        while use_git_ignore:
-            git_ignore = os.path.join(path, '.gitignore')
-            if os.path.isfile(git_ignore):
-                with open(git_ignore) as gi:
+        try:
+            filelist = check_output(['git', 'ls-files'], cwd=self.path).split()
+            self.to_include = [os.path.join(self.path, fn) for fn in filelist]
+        except CalledProcessError as err:
+            self.to_include = None
 
-                    to_ignore = [line.strip() for line in gi if not line.startswith('#') if line.strip()]
-                    self.to_ignore = ['**%s' % line if line[0] == '/' else '**/%s' % line for line in to_ignore]
-                    self.to_ignore.extend(line + '/' for line in self.to_ignore if line[-1] != '/')
-                break
-            next_path = os.path.dirname(path)
-            if next_path == path: break
-            path = next_path
+        self.num_included = 0
 
     def __call__(self, filename):
-        if '/.git/' in filename:
-            return True
-        if not self.to_ignore:
+        if self.to_include is None:
             return False
 
         if os.path.isdir(filename):
-            filename += '/'
-        if filename.startswith(self.path):
-            filename = filename[len(self.path):]
+            return False
 
-        return any(fnmatch(filename, pat) for pat in self.to_ignore)
+        if filename in self.to_include:
+            self.num_included += 1
+            return False
+
+        return True
