83099d698dadd6e2aee1e04616df31b072b5c43a
Phillip Cloud <cpcloud@gmail.com>
2015-04-15 14:07:06 -0400

    Fix tests

--- binstar_build_client/worker/tests/test_popen.py
+++ binstar_build_client/worker/tests/test_popen.py
@@ -1,6 +1,10 @@
 import unittest
 from binstar_build_client.worker.utils.buffered_io import BufferedPopen
-import io
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from io import StringIO
+
 import os
 skipWindows = unittest.skipIf(os.name == 'nt', 'This test does not run on windows')
 skipPosix = unittest.skipIf(os.name != 'nt', 'This test does not run on Posix')
@@ -9,14 +13,14 @@ class Test(unittest.TestCase):
 
 
     def test_stdout(self):
-        stdout = io.StringIO()
+        stdout = StringIO()
         p0 = BufferedPopen(['echo', 'hello'], stdout=stdout)
         self.assertEqual(p0.wait(), 0)
         self.assertEqual(stdout.getvalue().strip(), 'hello')
 
     @skipWindows
     def test_stdout_error(self):
-        stdout = io.StringIO()
+        stdout = StringIO()
         p0 = BufferedPopen(['bash', '-c', 'echo hello; bad-command'], stdout=stdout)
         return_code = p0.wait()
         self.assertNotEqual(return_code, 0)
@@ -24,7 +28,7 @@ class Test(unittest.TestCase):
 
     @skipPosix
     def test_stdout_error_win(self):
-        stdout = io.StringIO()
+        stdout = StringIO()
         p0 = BufferedPopen(['cmd', '/c', 'echo hello & bad-command'], stdout=stdout)
         return_code = p0.wait()
         self.assertNotEqual(return_code, 0)
@@ -33,14 +37,14 @@ class Test(unittest.TestCase):
 
     @skipWindows
     def test_stderr(self):
-        stdout = io.StringIO()
+        stdout = StringIO()
         p0 = BufferedPopen(['bash', '-c', 'echo stdout 2>&1; echo stderr 1>&2;'], stdout=stdout)
         self.assertEqual(p0.wait(), 0)
         self.assertEqual(stdout.getvalue(), 'stdout\nstderr\n')
 
     @skipWindows
     def test_iotimeout(self):
-        stdout = io.StringIO()
+        stdout = StringIO()
         p0 = BufferedPopen(['bash', '-c', 'echo hello && sleep 100 && echo goodby'], stdout=stdout, iotimeout=1)
         self.assertNotEqual(p0.wait(), 0)
         self.assertIn('hello', stdout.getvalue())
@@ -50,7 +54,7 @@ class Test(unittest.TestCase):
 
     @skipPosix
     def test_iotimeout_win(self):
-        stdout = io.StringIO()
+        stdout = StringIO()
         p0 = BufferedPopen(['cmd', '/c', 'echo hello && sleep 100 && echo goodby'], stdout=stdout, iotimeout=1)
         returncode = p0.wait()
         self.assertIn('hello', stdout.getvalue())
