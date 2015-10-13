b526cdeaa3fe1a0483892f653c262a3bc0a77184
srossross <srossross@gmail.com>
2015-10-13 15:11:14 -0700

    starting to refactor worker

--- binstar_build_client/commands/register.py
+++ binstar_build_client/commands/register.py
@@ -6,15 +6,13 @@ anaconda build register
 from __future__ import (print_function, unicode_literals, division,
     absolute_import)
 
-import os
 import platform
 
 from binstar_client import errors
 from binstar_client.utils import get_binstar
 
 from binstar_build_client import BinstarBuildAPI
-from binstar_build_client.worker.register import (register_worker,
-                                                  print_registered_workers,)
+from binstar_build_client.worker.register import WorkerConfiguration
 
 OS_MAP = {'darwin': 'osx', 'windows':'win'}
 ARCH_MAP = {'x86': '32',
@@ -24,12 +22,17 @@ ARCH_MAP = {'x86': '32',
             }
 
 def get_platform():
+    'Get the conda platform string of the current machine'
+
     operating_system = platform.system().lower()
     arch = platform.machine().lower()
     return '{}-{}'.format(OS_MAP.get(operating_system, operating_system),
                       ARCH_MAP.get(arch, arch))
 
 def get_dist():
+    '''
+    Get the current os and version
+    '''
     if platform.dist()[0]:
         return platform.dist()[0].lower()
     elif platform.mac_ver()[0]:
@@ -40,6 +43,10 @@ def get_dist():
     return 'unknown'
 
 def split_queue_arg(queue):
+    '''
+    Support old and new style queue
+    '''
+
     if queue.count('/') == 1:
         username, queue = queue.split('/', 1)
     elif queue.count('-') == 2:
@@ -50,7 +57,7 @@ def split_queue_arg(queue):
 
 def main(args):
     if args.list:
-        print_registered_workers()
+        WorkerConfiguration.print_registered_workers()
         return
 
     if not args.queue:
@@ -58,7 +65,8 @@ def main(args):
 
     args.username, args.queue = split_queue_arg(args.queue)
     bs = get_binstar(args, cls=BinstarBuildAPI)
-    return register_worker(bs, args)
+    worker_config = WorkerConfiguration.register(bs, args.username, args.queue, args.platform, args.hostname, args.dist)
+    worker_config.save()
 
 def add_parser(subparsers, name='register',
                description='Register a build worker to build jobs off of a binstar build queue',
@@ -85,11 +93,6 @@ def add_parser(subparsers, name='register',
     parser.add_argument('--dist', default=get_dist(),
                         help='The operating system distribution the worker should use (default: %(default)s)')
 
-    parser.add_argument('--cwd', default=os.path.abspath('.'), type=os.path.abspath,
-                        help='The root directory this build should use (default: "%(default)s")')
-    parser.add_argument('-t', '--max-job-duration', type=int, metavar='SECONDS',
-                        dest='timeout',
-                        help='Force jobs to stop after they exceed duration (default: %(default)s)', default=60 * 60)
     parser.set_defaults(main=main)
 
     return parser
