3d34eca44c2fcf7ca376278444bf181b55cdf8f3
srossross <srossross@gmail.com>
2015-10-19 14:05:28 -0700

    added timout tests

---
+++ binstar_build_client/worker/tests/test_timeout.py
@@ -0,0 +1,35 @@
+import unittest
+from binstar_build_client.worker.utils.timeout import Timeout
+
+class Test(unittest.TestCase):
+
+
+    def test_test_timeout(self):
+
+        @Timeout(.1)
+        def to():
+            to.callback_called = True
+            pass
+
+        with to:
+            while not to.timout_occurred:
+                pass
+
+        self.assertTrue(to.timout_occurred)
+        self.assertTrue(to.callback_called)
+
+    def test_test_timeout_2(self):
+        ''
+        @Timeout(2)
+        def to():
+            pass
+
+        with to:
+            pass
+
+        self.assertFalse(to.timout_occurred)
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.test_test_timer']
+    unittest.main()
