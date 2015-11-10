3df05bd33eb0ffe086eb0b368071d64c889d5abc
srossross <srossross@gmail.com>
2015-11-09 17:57:00 -0800

    Fix conda prefix lookup

--- binstar_build_client/utils/__init__.py
+++ binstar_build_client/utils/__init__.py
@@ -5,11 +5,14 @@ import os
 CONDA_EXE = 'conda.exe' if os.name == 'nt' else 'conda'
 
 def get_conda_root_prefix():
+    """
+    get the directory prefix to where conda is installed
+    """
+    canonical_dir_current_executable = os.path.dirname(os.path.realpath(sys.argv[0]))
+    paths = [canonical_dir_current_executable, ] + os.environ.get('PATH').split(os.pathsep)
 
-    for entry in os.environ.get('PATH').split(os.pathsep):
-
+    for entry in paths:
         if os.path.isdir(entry) and CONDA_EXE in os.listdir(entry):
             conda_exe_path = os.path.realpath(os.path.join(entry, 'conda'))
             bin_dir = os.path.dirname(conda_exe_path)
             return os.path.dirname(bin_dir)
-
--- binstar_build_client/worker_commands/run.py
+++ binstar_build_client/worker_commands/run.py
@@ -60,9 +60,13 @@ def add_parser(subparsers, name='run',
 
     dgroup = parser.add_argument_group('development options')
 
+    conda_prefix = get_conda_root_prefix()
+    if conda_prefix:
+        default_build_dir = os.path.join(conda_prefix, 'conda-bld', '{platform}')
+    else:
+        default_build_dir = None
     dgroup.add_argument("--conda-build-dir",
-                        default=os.path.join(get_conda_root_prefix(),
-                                             'conda-bld', '{platform}'),
+                        default=default_build_dir,
                         help="[Advanced] The conda build directory (default: %(default)s)",
                         )
 
