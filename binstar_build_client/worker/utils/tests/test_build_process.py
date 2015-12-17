b4f0c273ccd1877d60264a0c646e5de45e60a46a
srossross <srossross@gmail.com>
2015-12-17 13:01:13 -0800

    added process group specific tests

---
+++ binstar_build_client/worker/utils/tests/test_build_process.py
@@ -0,0 +1,83 @@
+import unittest
+from binstar_build_client.worker.utils.process_wrappers import BuildProcess, WIN_32
+import sys
+import os
+import time
+import psutil
+import signal
+
+module_dir = os.path.dirname(__file__)
+run_sub_process = os.path.join(module_dir, 'run_sub_process.py')
+
+class Test(unittest.TestCase):
+
+    def test_build_process_can_create_processes(self):
+
+        p0 = BuildProcess(['echo', 'hello'], '.')
+        p0.wait()
+
+        self.assertEqual(p0.readline().strip(), 'hello')
+        self.assertEqual(p0.readline().strip(), '')
+
+    def test_terminate_process_group_naive(self):
+
+
+        p0 = BuildProcess([sys.executable, run_sub_process], '.')
+        time.sleep(.1)
+
+        parent = psutil.Process(p0.pid)
+        children = parent.children()
+
+        os.kill(p0.pid, signal.SIGTERM)
+
+        p0.wait()
+
+        self.assertFalse(parent.is_running())
+        self.assertEqual([c.is_running() for c in children], [True])
+
+        for c in children:
+            if c.is_running(): c.kill()
+
+    @unittest.skipIf(WIN_32, 'This test should only run on posix')
+    def test_terminate_process_group(self):
+
+
+        p0 = BuildProcess([sys.executable, run_sub_process], '.')
+        time.sleep(.1)
+
+        parent = psutil.Process(p0.pid)
+        children = parent.children()
+
+        parent_pgid = os.getpgid(parent.pid)
+        for child in children:
+            self.assertEqual(parent_pgid, os.getpgid(child.pid))
+
+        p0.kill_pg()
+        p0.wait()
+        self.assertFalse(parent.is_running())
+        self.assertEqual([c.is_running() for c in children], [False])
+
+
+    @unittest.skipIf(not WIN_32, 'This test should only run on windows')
+    def test_terminate_job_object(self):
+
+        p0 = BuildProcess([sys.executable, run_sub_process], '.')
+        time.sleep(.1)
+
+        parent = psutil.Process(p0.pid)
+        children = parent.children()
+
+        parent_pgid = os.getpgid(parent.pid)
+        for child in children:
+            self.assertEqual(parent_pgid, os.getpgid(child.pid))
+
+        p0.kill_job()
+        p0.wait()
+        self.assertFalse(parent.is_running())
+        self.assertEqual([c.is_running() for c in children], [False])
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.test_build_process_can_create_processes']
+    unittest.main()
+
