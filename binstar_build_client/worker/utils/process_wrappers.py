015810d81ba9e8caea971e7c2712804a5201fc24
srossross <srossross@gmail.com>
2015-12-17 08:20:29 -0800

    don't want to name the object, could create conflicts

--- binstar_build_client/worker/utils/process_wrappers.py
+++ binstar_build_client/worker/utils/process_wrappers.py
@@ -41,7 +41,7 @@ class DockerBuildProcess(object):
 
 
 def create_job(hProcess):
-    hJob = win32job.CreateJobObject(None, "Anaconda-Build")
+    hJob = win32job.CreateJobObject(None, "")
     extended_info = win32job.QueryInformationJobObject(hJob, win32job.JobObjectExtendedLimitInformation)
     extended_info['BasicLimitInformation']['LimitFlags'] = win32job.JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE
     win32job.SetInformationJobObject(hJob, win32job.JobObjectExtendedLimitInformation, extended_info)
