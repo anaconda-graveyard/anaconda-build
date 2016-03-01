6ede9ce032b495e6f86a9f72d0744ecf3a289eaa
Derek Ludwig <dludwig@continuum.io>
2016-02-29 17:22:21 -0800

    Split working dir into staging and working dir.
    Working directory is where the executing process should `cd` into
    before execution. For the docker worker, this is relative to the
    container file system.
    
    Staging directory is where temporary files should be stored while
    setting up a build, on the host machine. For the regular worker, this is
    the same as the working directory.

--- binstar_build_client/worker/tests/test_jobs.py
+++ binstar_build_client/worker/tests/test_jobs.py
@@ -70,6 +70,7 @@ class MyWorker(Worker):
         args.status_file = None
         args.timeout = 100
         args.show_new_procs = False
+        args.cwd = tempfile.mkdtemp()
 
         worker_config = WorkerConfiguration(
             'worker_name',
@@ -77,12 +78,7 @@ class MyWorker(Worker):
             'test_platform', 'test_hostname', 'dist'
         )
 
-        self._working_dir = tempfile.mkdtemp()
         super(MyWorker, self).__init__(bs, worker_config, args)
-
-    def working_dir(self, *args):
-
-        return self._working_dir
 
 
 class Test(unittest.TestCase):
@@ -124,7 +120,7 @@ class Test(unittest.TestCase):
             st = os.stat(script_path)
             os.chmod(script_path, st.st_mode | stat.S_IEXEC)
 
-
+    maxDiff = None
     def get_worker(self):
         worker = MyWorker()
         return worker
@@ -293,6 +289,7 @@ class Test(unittest.TestCase):
         working_dir = tempfile.mkdtemp()
         try:
             script = script_generator.gen_build_script(working_dir,
+                                                       working_dir,
                                                        default_build_data())
             with open(script, 'r') as f:
                 contents = f.read()
@@ -310,6 +307,7 @@ class Test(unittest.TestCase):
             build_data['build_item_info']['instructions']['install_channels'] = []
             build_data['build_item_info']['engine'] = 'r'
             script = script_generator.gen_build_script(working_dir,
+                                                       working_dir,
                                                        build_data)
             with open(script, 'r') as f:
                 contents = f.read()
@@ -361,6 +359,7 @@ class TestDockerWorker(DockerWorker):
         args.timeout = 100
         args.show_new_procs = False
         args.image = 'binstar/linux-64'
+        args.cwd = tempfile.mkdtemp()
 
         worker_config = WorkerConfiguration(
             'worker_id', 'worker_id', 'username', 'queue', 'test_platform',
@@ -368,8 +367,6 @@ class TestDockerWorker(DockerWorker):
 
         super(TestDockerWorker, self).__init__(bs, worker_config, args)
 
-    def working_dir(self, *args):
-        return os.path.abspath('test_worker')
 
 
 @unittest.skipIf(not have_docker(), "Don't have docker")
