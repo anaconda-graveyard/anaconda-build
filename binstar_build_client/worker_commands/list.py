cc6f584a13cabb6497ac641abf0bcb70ffdc3b93
Peter Steinberg <psteinberg@continuum.io>
2016-03-04 06:31:33 -0500

    fix anaconda worker list with more args

--- binstar_build_client/worker_commands/list.py
+++ binstar_build_client/worker_commands/list.py
@@ -6,11 +6,13 @@ anaconda worker list
 from __future__ import (print_function, unicode_literals, division,
     absolute_import)
 
+import logging
+
 from binstar_build_client.worker.register import WorkerConfiguration
 from binstar_client.utils import get_binstar
 from binstar_build_client import BinstarBuildAPI
+from binstar_build_client.worker.register import split_queue_arg
 
-import logging
 log = logging.getLogger('binstar.build')
 
 def print_registered_workers(bs, args):
@@ -18,16 +20,17 @@ def print_registered_workers(bs, args):
     has_workers = False
 
     log.info('Registered workers:')
-
+    if args.queue:
+        user, args.queue = split_queue_arg(args.queue)
     for wconfig in WorkerConfiguration.registered_workers(bs):
         has_workers = True
         if args.this_host_only and wconfig.hostname != WorkerConfiguration.HOSTNAME:
             continue
-        if args.queue and args.queue != wconfig:
+        if args.queue and args.queue != wconfig.queue:
             continue
         if args.org and args.org != wconfig.username:
             continue
-        msg = '{name}, id:{worker_id}, hostname:{hostname}, queue:{username}/{queue}'.format(name=wconfig.name, **wconfig.to_dict())
+        msg = '{name}, id:{worker_id}, hostname:{hostname}, queue:{username}/{queue}'.format(**wconfig.to_dict())
         if wconfig.pid:
             msg += ' (running with pid: {})'.format(wconfig.pid)
 
@@ -39,7 +42,7 @@ def print_registered_workers(bs, args):
 def main(args):
 
     bs = get_binstar(args, cls=BinstarBuildAPI)
-    print_registered_workers(bs, this_host_only=args.this_host_only)
+    print_registered_workers(bs, args)
 
 def add_parser(subparsers, name='list',
                description='List build workers and queues',
@@ -50,6 +53,7 @@ def add_parser(subparsers, name='list',
                                    )
     parser.add_argument('--this-host-only',
                         '-t',
+                        action='store_true',
                         help="Print only workers registered from this hostname.")
     parser.add_argument('--org',
                         '-o',
