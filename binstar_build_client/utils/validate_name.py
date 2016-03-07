2787a5905db7b99b57717418888b6692bbebdc7c
Peter Steinberg <psteinberg@continuum.io>
2016-03-07 15:17:27 -0800

    remove outdated part of docstring

--- binstar_build_client/utils/validate_name.py
+++ binstar_build_client/utils/validate_name.py
@@ -5,7 +5,7 @@ from binstar_client import errors
 PATTERN = '^[a-zA-Z]+[a-zA-Z\-_0-9]*$'
 
 def is_valid_name(name):
-    '''is_valid_name(name, context, allow_non_letters='')
+    '''is_valid_name(name)
     Used to reject queue names like
 
     orgname/queue!
